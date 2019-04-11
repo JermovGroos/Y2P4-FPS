679565a16b774bac3a7aab06f6ef54d2ea2c227a
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-04-11 14:37:41 +0200

    Update
    -Temp movement && Temp Weapon script
    -Killing works
    -KillFeed
    -Leave Serialization

---
+++ FPS/Assets/Scripts/Ingame/_Temp/TempGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGun : MonoBehaviour
+{
+    public float range;
+    public float damage;
+    public string playerTag;
+    public TempMovement movement;
+
+    public void Update()
+    {
+        if (movement.isMine)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+                    if (hit.transform.gameObject.tag == playerTag)
+                        hit.transform.GetComponent<PhotonView>().RPC("Damage", PhotonTargets.All, damage, PhotonNetwork.playerName);
+            }
+        }
+    }
+}
