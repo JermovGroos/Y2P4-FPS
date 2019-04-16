f9ff565b4984b31375b0696e8c8d14ccfc1f7ec1
Remasu <stefanherder2001@gmail.com>
2019-04-16 16:53:20 +0200

    Worked on the bullet impacts

---
+++ FPS/Assets/VFX/ImpactTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactTester : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }    
+    }
+
+    public void Fire()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            GameObject decal = Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up,hit.normal));
+            Destroy(decal, 10f);
+        }
+    }
+}
