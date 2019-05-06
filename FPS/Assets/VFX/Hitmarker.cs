810b091e8acaf82b0af9e5fb06caa9f200d83bf0
Remasu <stefanherder2001@gmail.com>
2019-05-06 10:53:48 +0200

    Added hitmarker
    to test press right mouse button in the VFXhoekje.unity scene

---
+++ FPS/Assets/VFX/Hitmarker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hitmarker : MonoBehaviour
+{
+    public Image hitmarker;
+
+    public AnimationCurve opacityOverTime;
+    private float _time = 50;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            HitMarker();
+        }
+
+        _time += 1 * Time.deltaTime;
+
+        hitmarker.color = new Color(255,255,255,opacityOverTime.Evaluate(_time));
+
+    }
+
+    public void HitMarker()
+    {
+        _time = 0;
+    }
+}
