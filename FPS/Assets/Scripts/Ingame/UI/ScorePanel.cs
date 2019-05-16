6e497a6812883c3f6161f712d957788348c52b93
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-05-16 15:10:03 +0200

    Added Score Panel script

---
+++ FPS/Assets/Scripts/Ingame/UI/ScorePanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScorePanel : MonoBehaviour
+{
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI kills;
+    public TextMeshProUGUI deaths;
+    public TextMeshProUGUI assists;
+    public TextMeshProUGUI ratio;
+    public TextMeshProUGUI ping;
+}
