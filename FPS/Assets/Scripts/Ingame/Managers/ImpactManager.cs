a2c11ff8d0e0d24e370c6a34217d0fa893dd3124
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-05-20 14:32:33 +0200

    Impact particles

---
+++ FPS/Assets/Scripts/Ingame/Managers/ImpactManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactManager : Photon.MonoBehaviour
+{
+    public SoundImpact[] impactTypes;
+    public int baseParticle;
+    public float particleLifetime;
+
+    public void SendImpactInfo(PhysicMaterial type, RaycastHit hit)
+    {
+        bool found = false;
+        for (int i = 0; i < impactTypes.Length; i++)
+            if (impactTypes[i].materialType.name + " (Instance)" == type.name)
+            {
+                found = true;
+                photonView.RPC("SpawnParticle", PhotonTargets.All, i, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+                break;
+            }
+        if (!found)
+            photonView.RPC("SpawnParticle", PhotonTargets.All, baseParticle, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+    }
+
+    [PunRPC]
+    public void SpawnParticle(int index, Vector3 pos, Quaternion rot)
+    {
+        Destroy(Instantiate(impactTypes[index].particle, pos, rot),particleLifetime);
+    }
+
+    [System.Serializable]
+    public class SoundImpact
+    {
+        public PhysicMaterial materialType;
+        public GameObject particle;
+    }
+}
---
+++ FPS/Assets/Scripts/Ingame/Player/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum FireTypes { SingleFire,Burst,Auto}
+    public FireTypes fireType;
+    public float damage;
+    public float fireRate;
+    public float spread;
+    public int clipSize;
+    [Range(1,20)]
+    public int bulletAmount;
+
+    public string playerTag;
+    public string managerTag;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+
+    public void Fire()
+    {
+        for (int i = 0; i < bulletAmount; i++)
+        {
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(transform.position, transform.forward ,out hit, Mathf.Infinity))
+                GameObject.FindWithTag(managerTag).GetComponent<ImpactManager>().SendImpactInfo(hit.collider.material, hit);
+        }
+    }
+}
