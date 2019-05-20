03628baf828db2cb67914397b886101bf968b193
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-05-20 13:19:55 +0200

    Updated ScorePanel script

--- FPS/Assets/Scripts/Ingame/UI/ScorePanel.cs
+++ FPS/Assets/Scripts/Ingame/UI/ScorePanel.cs
@@ -10,6 +10,21 @@ public class ScorePanel : MonoBehaviour
     public TextMeshProUGUI kills;
     public TextMeshProUGUI deaths;
     public TextMeshProUGUI assists;
-    public TextMeshProUGUI ratio;
     public TextMeshProUGUI ping;
+
+    // public GameInfoManager.PlayerInfo playerInfo;
+
+
+
+    void SetInfo(GameInfoManager.PlayerInfo info)
+    {
+
+        //Set info of the player
+        playerName.text = info.playerInfo.NickName;
+        score.text = "SCORE"; 
+        kills.text = info.kills.ToString();
+        deaths.text = info.deaths.ToString();
+        assists.text = info.assists.ToString();
+
+    }
 }
