1dfec7b40cb87c985dcc194e0532438235eafc2b
Remasu <stefanherder2001@gmail.com>
2019-05-20 16:00:28 +0200

    Added optione

---
+++ FPS/Assets/Scripts/Ingame/UI/ToggleInGameOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToggleInGameOptions : MonoBehaviour
+{
+    public GameObject options;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            options.SetActive(!options.activeInHierarchy);
+        }
+    }
+
+    public void Return()
+    {
+        options.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
