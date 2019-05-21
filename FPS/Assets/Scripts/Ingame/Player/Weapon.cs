324283ec2c100fe1481970a801c0a041fd946270
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-05-21 14:22:09 +0200

    Weapon Stuff

--- FPS/Assets/Scripts/Ingame/Player/Weapon.cs
+++ FPS/Assets/Scripts/Ingame/Player/Weapon.cs
@@ -12,26 +12,41 @@ public class Weapon : MonoBehaviour
     public int clipSize;
     [Range(1,20)]
     public int bulletAmount;
+    public float burstDelay;
 
     public string playerTag;
     public string managerTag;
 
     public ParticleSystem p;
 
+    bool allowFire = true;
+
     public void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-            Fire();
+        if (Input.GetButtonDown("Fire1") && fireType == FireTypes.SingleFire && allowFire || Input.GetButtonDown("Fire1") && fireType == FireTypes.Burst && allowFire)
+            StartCoroutine(Fire(fireRate));
+        else if (Input.GetButton("Fire1") && fireType == FireTypes.Auto && allowFire)
+            StartCoroutine(Fire(fireRate));
     }
 
-    public void Fire()
+    public IEnumerator Fire(float time)
     {
+        allowFire = false;
         p.Play();
         for (int i = 0; i < bulletAmount; i++)
         {
+            Vector3 addDirection = new Vector3(Random.Range(-spread, spread), Random.Range(-spread, spread), Random.Range(-spread, spread));
             RaycastHit hit = new RaycastHit();
-            if (Physics.Raycast(transform.position, transform.forward ,out hit, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, transform.forward + addDirection ,out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == playerTag)
+                    hit.transform.GetComponent<Player>().DamagePlayer(PhotonNetwork.playerName, damage);
                 GameObject.FindWithTag(managerTag).GetComponent<ImpactManager>().SendImpactInfo(hit.collider.material, hit);
+            }
+            if (fireType == FireTypes.Burst)
+                yield return new WaitForSeconds(burstDelay);
         }
+        yield return new WaitForSeconds(time);
+        allowFire = true;
     }
 }
