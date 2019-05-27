ecb373e039b7a6d5e89f06c6e21ebadb6a317a30
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-05-27 13:26:05 +0200

    Scar Implemented, and otherThings

--- FPS/Assets/Scripts/MainMenu/Customization/Weapons/WeaponCustomizer.cs
+++ FPS/Assets/Scripts/MainMenu/Customization/Weapons/WeaponCustomizer.cs
@@ -7,6 +7,8 @@ public class WeaponCustomizer : MonoBehaviour
     public WeaponCustomizationLayout layout;
     public Transform showLocation;
     public int currentWeapon = 0;
+    public int currentBarrel = 0;
+    public int currentMagazine = 0;
     public GameObject currentItem;
 
     public void Start()
@@ -19,5 +21,34 @@ public class WeaponCustomizer : MonoBehaviour
         if (currentItem)
             Destroy(currentItem);
         currentItem = Instantiate(layout.weapons[currentWeapon].baseWeapon, showLocation.position, showLocation.rotation);
+
+        WeaponCustomizations customization = currentItem.GetComponent<WeaponCustomizations>();
+
+        for (int i = 0; i < customization.barrels.Length; i++)
+            if (i == currentBarrel)
+                customization.barrels[i].SetActive(true);
+            else
+                customization.barrels[i].SetActive(false);
+
+        for (int i = 0; i < customization.magazines.Length; i++)
+            if (i == currentMagazine)
+                customization.magazines[i].SetActive(true);
+            else
+                customization.magazines[i].SetActive(false);
+    }
+
+    public void ChangeWeapon(int i)
+    {
+
+    }
+
+    public void ChangeBarrel(int i)
+    {
+
+    }
+
+    public void ChangeMagazine(int i)
+    {
+
     }
 }
