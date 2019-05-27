f59eb575ae769ba01b5fa400423952f7b0ea1c5b
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-05-27 10:11:54 +0200

    PrefabChange and reimported the weapons

--- FPS/Assets/Scripts/MainMenu/Customization/Weapons/WeaponCustomizer.cs
+++ FPS/Assets/Scripts/MainMenu/Customization/Weapons/WeaponCustomizer.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class WeaponCustomizer : MonoBehaviour
 {
     public WeaponCustomizationLayout layout;
+    public Transform showLocation;
+    public int currentWeapon = 0;
+    public GameObject currentItem;
+
+    public void Start()
+    {
+        DisplayCurrentThings();
+    }
+
+    public void DisplayCurrentThings()
+    {
+        if (currentItem)
+            Destroy(currentItem);
+        currentItem = Instantiate(layout.weapons[currentWeapon].baseWeapon, showLocation.position, showLocation.rotation);
+    }
 }
---
+++ FPS/Assets/Scripts/MainMenu/_Temp/CustomizationSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizationSwitch : MonoBehaviour
+{
+    public void Open()
+    {
+        Camera.main.transform.position += Vector3.right * 5;
+    }
+
+    public void Close()
+    {
+        Camera.main.transform.position -= Vector3.right * 5;
+    }
+}
