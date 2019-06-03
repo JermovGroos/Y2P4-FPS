d06cc194886ac92ad7b6b916895482578e204fab
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-06-03 13:56:08 +0200

    Added quick CursorLock script

---
+++ FPS/Assets/Scripts/CursorLock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour
+{
+
+    bool locked = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(locked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                locked = !locked;
+            } else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                locked = !locked;
+            }
+        }
+    }
+}
