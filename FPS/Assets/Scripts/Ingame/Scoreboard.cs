656368fe5f89a340583de02b961a1600af4acb0b
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-06-12 13:32:14 +0200

    Added sorting to scoreboard

--- FPS/Assets/Scripts/Ingame/Scoreboard.cs
+++ FPS/Assets/Scripts/Ingame/Scoreboard.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 
@@ -31,12 +32,7 @@ public class Scoreboard : MonoBehaviour
                 uiPanels.RemoveAt(i - 1);
             }
 
-            //Instantiate score panels
-            for (int i = 0; i < manager.team1.players.Count; i++)
-            {
-
-                //Get info
-                GameInfoManager.PlayerInfo info = manager.team1.players[i];
+            foreach(GameInfoManager.PlayerInfo info in manager.team1.players.OrderBy(x => x.kills)) {
 
                 //Create panel
                 string playerName = info.playerInfo.NickName;
@@ -53,15 +49,9 @@ public class Scoreboard : MonoBehaviour
 
                 //Add it to the list
                 uiPanels.Add(newPanel);
-
             }
 
-            //Instantiate score panels
-            for (int i = 0; i < manager.team2.players.Count; i++)
-            {
-
-                //Get info
-                GameInfoManager.PlayerInfo info = manager.team2.players[i];
+            foreach (GameInfoManager.PlayerInfo info in manager.team2.players.OrderBy(y => y.kills)) {
 
                 //Create panel
                 string playerName = info.playerInfo.NickName;
@@ -70,7 +60,7 @@ public class Scoreboard : MonoBehaviour
                 string assists = info.assists.ToString();
 
                 //Instantiate panel and set text
-                GameObject newPanel = Instantiate(panelPrefab, team2PanelParent.transform);
+                GameObject newPanel = Instantiate(panelPrefab, team1PanelParent.transform);
                 newPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = playerName;
                 newPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = kills;
                 newPanel.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = deaths;
@@ -78,8 +68,59 @@ public class Scoreboard : MonoBehaviour
 
                 //Add it to the list
                 uiPanels.Add(newPanel);
-
             }
+
+
+
+            ////Instantiate score panels
+            //for (int i = 0; i < manager.team1.players.Count; i++)
+            //{
+
+            //    //Get info
+            //    GameInfoManager.PlayerInfo info = manager.team1.players[i];
+
+            //    //Create panel
+            //    string playerName = info.playerInfo.NickName;
+            //    string kills = info.kills.ToString();
+            //    string deaths = info.deaths.ToString();
+            //    string assists = info.assists.ToString();
+
+            //    //Instantiate panel and set text
+            //    GameObject newPanel = Instantiate(panelPrefab, team1PanelParent.transform);
+            //    newPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = playerName;
+            //    newPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = kills;
+            //    newPanel.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = deaths;
+            //    newPanel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = assists;
+
+            //    //Add it to the list
+            //    uiPanels.Add(newPanel);
+
+            //}
+
+            ////Instantiate score panels
+            //for (int i = 0; i < manager.team2.players.Count; i++)
+            //{
+
+            //    //Get info
+            //    GameInfoManager.PlayerInfo info = manager.team2.players[i];
+
+            //    //Create panel
+            //    string playerName = info.playerInfo.NickName;
+            //    string kills = info.kills.ToString();
+            //    string deaths = info.deaths.ToString();
+            //    string assists = info.assists.ToString();
+
+            //    //Instantiate panel and set text
+            //    GameObject newPanel = Instantiate(panelPrefab, team2PanelParent.transform);
+            //    newPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = playerName;
+            //    newPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = kills;
+            //    newPanel.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = deaths;
+            //    newPanel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = assists;
+
+            //    //Add it to the list
+            //    uiPanels.Add(newPanel);
+
+            //}
 
             //Wait for update
             yield return new WaitForSeconds((float)hertz / 60);
