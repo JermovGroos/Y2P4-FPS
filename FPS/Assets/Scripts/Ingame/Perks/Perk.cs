aea289614bba2bcdee44592796f9612f7eb2774b
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-06-12 16:32:30 +0200

    Added Perk foundation

---
+++ FPS/Assets/Scripts/Ingame/Perks/Perk.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Perk", menuName = "Perk")]
+public class Perk : ScriptableObject
+{
+    public string className; //Name of the class
+    public string variableName; //Name of the variable
+    public float toAdd = 0; //Value to add to the variable
+    public float toMultiply = 1; //Value to multiply the variable with
+}
---
+++ FPS/Assets/Scripts/Ingame/Perks/PerkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class PerkManager : MonoBehaviour
+{
+    public List<Perk> perks = new List<Perk>();
+
+    public void ApplyPerk(GameObject holder, Perk perk)
+    {
+        //Get value of requested variable
+        Component component = holder.GetComponent(perk.className); //Get component
+        FieldInfo field = component.GetType().GetField(perk.variableName); //Get field
+        float value = (float)field.GetValue(component); //Get value
+
+        //Add value to said variable
+        value *= perk.toMultiply; //Multiply current value with perk multiplier
+        value += perk.toAdd; //Add perk value to current value
+
+        //Set new value
+        field.SetValue(component, value); //Set variable
+    }
+}
