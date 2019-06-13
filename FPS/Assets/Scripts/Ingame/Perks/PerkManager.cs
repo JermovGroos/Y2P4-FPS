2fe3e3151a0295e00dccda3affacc47d4a15f4dd
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-06-13 12:43:35 +0200

    Added ability to remove perks

--- FPS/Assets/Scripts/Ingame/Perks/PerkManager.cs
+++ FPS/Assets/Scripts/Ingame/Perks/PerkManager.cs
@@ -9,28 +9,51 @@ public class PerkManager : MonoBehaviour
 
     public void ApplyPerk(GameObject holder, Perk perk)
     {
-        //Get value of requested variable
-        Component component = holder.GetComponent(perk.className); //Get component
-        FieldInfo field = component.GetType().GetField(perk.variableName); //Get field
-        float oldValue = (float)field.GetValue(component); //Get value
+        if (!activePerks.Contains(perk))
+        {
+            //Get value of requested variable
+            Component component = holder.GetComponent(perk.className); //Get component
+            FieldInfo field = component.GetType().GetField(perk.variableName); //Get field
+            float oldValue = (float)field.GetValue(component); //Get value
 
-        //Add value to said variable
-        float newValue = oldValue; //Create new value variable
-        newValue *= perk.toMultiply; //Multiply current value with perk multiplier
-        newValue += perk.toAdd; //Add perk value to current value
+            //Add perk value to said variable
+            float newValue = oldValue; //Create new value variable
+            newValue *= perk.toMultiply; //Multiply current value with perk multiplier
+            newValue += perk.toAdd; //Add perk value to current value
 
-        //Set new value
-        field.SetValue(component, oldValue); //Set variable
+            //Set new value
+            field.SetValue(component, newValue); //Set variable
 
-        //Add perk to active perks
-        activePerks.Add(perk); //Add perk to active perks
+            //Add perk to active perks
+            activePerks.Add(perk); //Add perk to active perks
 
-        //Log to console
-        print("Successfully changed " + component.name + "." + field.Name + " on " + holder.name + " from " + oldValue + " to " + newValue);
+            //Log to console
+            print("Successfully changed " + component.name + "." + field.Name + " on " + holder.name + " from " + oldValue + " to " + newValue);
+        }
     }
 
     public void RemovePerk(GameObject holder, Perk perk)
     {
+        if (activePerks.Contains(perk))
+        {
+            //Get value of requested variable
+            Component component = holder.GetComponent(perk.className); //Get component
+            FieldInfo field = component.GetType().GetField(perk.variableName); //Get field
+            float oldValue = (float)field.GetValue(component); //Get value
 
+            //Remove perk value from said variable
+            float newValue = oldValue; //Create new value variable
+            newValue /= perk.toMultiply; //Devide new value by perk multiplication
+            newValue -= perk.toAdd; //Subtract new value by perk addition
+
+            //Set new value
+            field.SetValue(component, newValue); //Set variable
+
+            //Remove perk from active perks
+            activePerks.Remove(perk);
+
+            //Log to console
+            print("Successfully changed " + component.name + "." + field.Name + " on " + holder.name + " from " + oldValue + " to " + newValue);
+        }
     }
 }
