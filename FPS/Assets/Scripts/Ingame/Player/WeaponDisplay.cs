40cd89c59161627d80c4844a0c1552ed21d8b8e9
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-06-20 13:53:03 +0200

    MIKEY TESTEN

--- FPS/Assets/Scripts/Ingame/Player/WeaponDisplay.cs
+++ FPS/Assets/Scripts/Ingame/Player/WeaponDisplay.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class WeaponDisplay : Photon.MonoBehaviour
 {
     public WeaponCustomizationLayout layout;
-    public WeaponCustomizations weapon;
+    public WeaponCustomizations weapon, tempWeapon;
     public Transform weaponLocation;
     public Weapon weapons;
+    public Transform otherPos;
 
     public void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
     {
@@ -35,24 +36,46 @@ public class WeaponDisplay : Photon.MonoBehaviour
     {
         if(weapon)
             weapon.muzzleFlash.GetComponent<ParticleSystem>().Play();
+        if (tempWeapon)
+            weapon.muzzleFlash.GetComponent<ParticleSystem>().Play();
     }
 
     [PunRPC]
     public void DisplayWeapon(int _weapon, int _barrel, int _magazine)
     {
-        foreach (Transform chid in weaponLocation)
-            Destroy(chid.gameObject);
-        weapon = Instantiate(layout.weapons[_weapon].baseWeapon, weaponLocation).GetComponent<WeaponCustomizations>();
-        for (int i = 0; i < weapon.barrels.Length; i++)
-            if (i == _barrel)
-                weapon.barrels[i].SetActive(true);
-            else
-                weapon.barrels[i].SetActive(false);
+        if (!weaponLocation.gameObject.activeInHierarchy)
+        {
+            foreach (Transform chid in otherPos)
+                Destroy(chid.gameObject);
+            tempWeapon = Instantiate(layout.weapons[_weapon].baseWeapon, weaponLocation).GetComponent<WeaponCustomizations>();
+            for (int i = 0; i < tempWeapon.barrels.Length; i++)
+                if (i == _barrel)
+                    tempWeapon.barrels[i].SetActive(true);
+                else
+                    tempWeapon.barrels[i].SetActive(false);
 
-        for (int i = 0; i < weapon.magazines.Length; i++)
-            if (i == _magazine)
-                weapon.magazines[i].SetActive(true);
-            else
-                weapon.magazines[i].SetActive(false);
+            for (int i = 0; i < tempWeapon.magazines.Length; i++)
+                if (i == _magazine)
+                    tempWeapon.magazines[i].SetActive(true);
+                else
+                    tempWeapon.magazines[i].SetActive(false);
+        }
+        else
+        {
+            foreach (Transform chid in weaponLocation)
+                Destroy(chid.gameObject);
+            weapon = Instantiate(layout.weapons[_weapon].baseWeapon, weaponLocation).GetComponent<WeaponCustomizations>();
+            for (int i = 0; i < weapon.barrels.Length; i++)
+                if (i == _barrel)
+                    weapon.barrels[i].SetActive(true);
+                else
+                    weapon.barrels[i].SetActive(false);
+
+            for (int i = 0; i < weapon.magazines.Length; i++)
+                if (i == _magazine)
+                    weapon.magazines[i].SetActive(true);
+                else
+                    weapon.magazines[i].SetActive(false);
+        }
     }
 }
